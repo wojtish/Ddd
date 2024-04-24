a0464f67ae5c5dbe026d4ab50d89330605b72883
wojtish <20014605+wojtish@users.noreply.github.com>
2024-04-24 18:08:17 +0200

    Update inside_bez_momentum.js

--- inside_bez_momentum.js
+++ inside_bez_momentum.js
@@ -1,1 +1,12 @@
+function getRandomInteger(max) {
+    return Math.floor(Math.random() * max);
+}
 
+let entries = lib().entries();
+let numberOfEntries = entries.length;
+let selection =
+    getRandomInteger(numberOfEntries);
+let e = entries[selection];
+// Put code to show this entry here
+
+e.show()
