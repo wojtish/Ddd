775e98208988394417a74f8d78c244937e4552ce
wojtish <20014605+wojtish@users.noreply.github.com>
2024-04-27 18:04:25 +0200

    Update search_dwa_buttony_important_biblia.js

--- search_dwa_buttony_important_biblia.js
+++ search_dwa_buttony_important_biblia.js
@@ -6,67 +6,40 @@ ui().layout([
 ui().layout([
 
 ui().button('in').action(function() {libByName("inside").show()}).width(200),
-ui().button('im').action(function() {libByName("important").show()}).width(150),
-ui().button('mem').action(function() {libByName("memento open").show()}).width(170),
+ui().button('im').action(function() {libByName("important").show()}).width(200),
+ui().button('mem').action(function() {libByName("memento open").show()}).width(200),
 ui().button('lib').action(function() {lib().show()}).width(190)
-
-
-
 ]).horizontal(),
-
 
 ui().layout([
 ui().button('szukaj dz.').action(function() { 
-
-
 //var libs = libByName("important").entries()
 //var names = libs.field("nazwa")
 var nr = ui().findByTag('name2').text
 if (nr == ""){
 nr = 0}
-
-
 var search1 = libByName("dziennik").find(ui().findByTag('name').text) 
 //for (i = 0; i < libs.length; i++) {
 //var search1 =libByName(libs[i].field("nazwa")).find(ui().findByTag('name').text) }
-
 message(search1.length);
-
-
-
-
-
 search1[nr].show()
         return true; 
-    }).width(250),
+    }).width(300),
 
-
-ui().button('szukaj w biblii').action(function() { 
-        
+ui().button('szukaj w biblii').action(function() {       
 var lib1 = libByName("Biblia audio memento");
 var search1 = lib1.find(ui().findByTag('name').text)
-
 var tyu = search1[0].field("nazwa")
 message(search1.length);
 search1[nr].show()
         return true; 
-    }).width(340),
-
+    }).width(430),
 ui().edit('').tag('name2').width(50), 
-
 ]).horizontal(),
-
-
-
 
 ui().layout([
-
-
 ui().edit('').tag('name')
-
 ]).horizontal(),
-
-
 
 ui().layout([
 ui().button('jas').action(function() {
@@ -76,23 +49,15 @@ i.send();
 }).width(200),
 
 ui().button('mo').action(function() {
-
-
 var min = Math.ceil(10000)
 var max = Math.floor(1000)
-
 for (var ent = 0; ent < lib().entries().length; ent++){    
 
 var ggg = Math.floor(Math.random() * (max - min)) + min
 var jjj = ggg / 100
 var kkk = jjj.toFixed(2)
-
 lib().entries()[ent].set("czas momentum",kkk)
-
 }
-
-
-
 }).width(200),
 
 ui().button('zap').action(function() {
@@ -106,8 +71,6 @@ return true;
 }
 ).width(200),
 
-
-
 ui().button('ins').action(function() {
 function getRandomInteger(max) {
     return Math.floor(Math.random() * max);
@@ -118,33 +81,34 @@ let selection =
     getRandomInteger(numberOfEntries);
 let e = entries[selection];
 // Put code to show this entry here
-
 e.show()
 }).width(190),
-
 ]).horizontal(),
+
+ui().layout([
+ui().button('podp.').action(function() { 
+entry().images("zdjęcie")[0].caption = ui().findByTag('name').text
+message(entry().images("zdjęcie")[0].caption)
+entry().show()
+        return true; 
+    }).width(200),
 
 ui().button('zablokuj').action(function() {var url = ("tasker://secondary?text=zablokujekran");
 var i = intent("android.intent.action.VIEW"); i.data(url);
-i.send();}).width(777),
+i.send();}).width(585),
 
+]).horizontal(),
 
 ui().button('szukaj w im').action(function() { 
 var nr = ui().findByTag('name2').text
 if (nr == ""){
 nr = 0}
-
 var lib1 = libByName("important");
-
 var search1 = lib1.find(ui().findByTag('name').text)
-
 message(search1.length);
-
 libByName(search1[nr].field("nazwa")).show()
-
         return true; 
-    }).width(779).height(300),
-
+    }).width(785).height(300),
 ])
 )
 .show()
