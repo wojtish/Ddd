3f6554b0a9d074fba8964f12b6714405a78de52c
wojtish <20014605+wojtish@users.noreply.github.com>
2024-04-27 13:22:17 +0200

    Create zmien_zdjecie_Minus_jeden_image_shiifting.js

---
+++ zmien_zdjecie_Minus_jeden_image_shiifting.js
@@ -0,0 +1,61 @@
+
+var e = entry();
+
+// Name of Primary field
+
+const images = "zdjęcie";  
+const integer = "integer";  
+
+
+var prime = e.field(images);
+
+if (prime[0] == null) {
+  exit();
+} 
+
+
+var imgPrime = e.images(images);
+var capt = imgPrime.map(a => a.caption);
+
+var prevCount = parseInt(e.field(integer));
+
+var imageCount = prime.length;
+
+
+var index = prevCount % imageCount;
+
+if (index == 0) {
+  e.set(integer, 1);
+  exit();
+} 
+
+if (index < 0) {
+  index += imageCount;
+}  
+
+
+var newPrime = shiftArray(index, prime);
+var newCapt = shiftArray(index, capt);
+
+e.set(integer, -1);
+e.set(images, newPrime);
+
+
+imgPrime = e.images(images);
+
+for (k in imgPrime) {
+
+  imgPrime[k].caption = newCapt[k];
+
+} 
+
+
+
+function shiftArray(ind, arr) {
+
+  var ar1 = arr.slice(- ind).reverse();
+  var ar2 = arr.slice(0, imageCount - ind).reverse();
+
+  return [].concat(ar2, ar1).reverse(); 
+
+}
